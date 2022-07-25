73cb7b0c66e02cb678cc763e4a6d2667a828883b
Alexey Tarasov <tarasovion2004@gmail.com>
2022-07-25 23:10:15 +0300

    Codeforces 50A

---
+++ Codeforces/problems/50A.cpp
@@ -0,0 +1,15 @@
+/* 50A. Domino piling  */
+// https://codeforces.com/problemset/problem/50/A
+// Date: Jul/25/2022 23:09 	
+// Run time: 30 ms
+// Memory: 0 KB
+// Verdict: AC
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    cout << n * m / 2 << endl;
+}
