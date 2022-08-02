d6c1eb226ef45b4bb69fd90dae1c2d138ce18adb
Alexey Tarasov <tarasovion2004@gmail.com>
2022-08-02 13:33:15 +0300

    Codeforces: The 2022 Hangzhou Normal U Summer Trials (problem A)

---
+++ Codeforces/problems/hello-acmer.py
@@ -0,0 +1,9 @@
+''' The 2022 Hangzhou Normal U Summer Trials: A. Hello, ACMer! '''
+# https://codeforces.com/gym/103736/problem/A
+# Date: Aug/02/2022 13:32
+# Run time: 61 ms
+# Memory: 0 KB
+# Verdict: AC
+
+s = input()
+print(s.count('hznu'))
