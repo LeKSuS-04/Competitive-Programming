46281ec5fc89afd6c329f7d34fa8f5051d68ead8
Alexey Tarasov <tarasovion2004@gmail.com>
2022-10-08 16:21:36 +0300

    AtCoder Beginner Contest 272

---
+++ AtCoder/AtCoder Beginner Contest 272/A.py
@@ -0,0 +1,9 @@
+''' A - Integer Sum '''
+# https://atcoder.jp/contests/abc272/tasks/abc272_a
+# Date: 2022-10-08 15:01:26
+# Run time: 25 ms
+# Memory: 9020 KB
+# Verdict: AC
+
+_ = input()
+print(sum(list(map(int, input().split()))))
